131c3b32cad63f60862f0fcb01e8f0ad56a66b31
Vincent Selz <s6viselz@uni-bonn.de>
2020-06-30 18:34:03 +0200

    Function to call datasets.

--- auxiliary/overview.py
+++ auxiliary/overview.py
@@ -1,6 +1,21 @@
-"""This module replicates Table 1 of the paper."""
+"""This module consists of some preliminary functions for data analysis."""
 
 import pandas as pd
+
+def get_datasets():
+    """Quickly get datasets necessary for replication.
+
+
+
+    Returns: Two datasets: One for the first movers and another for the second movers.
+    """
+    df_2 = pd.read_stata('data/paper_data/master_data.dta')
+    df_1 = pd.read_stata('data/paper_data/master_data_FM.dta')
+    df_2.columns = map(str.lower, df_2.columns)
+    df_2 = df_2.set_index(['subject','period'])
+    df_2_pref = df_2.drop(index=302)
+    return df_1, df_2, df_2_pref
+
 
 def effort_overview(df_2):
     """Gives overview over efforts.
@@ -13,11 +28,10 @@ def effort_overview(df_2):
 
     Return: Dataframe with an overview of the effort choices for the respective round.
     """
-    df_2_pref = df_2.drop(index=302)
     rslt = pd.DataFrame()
     for name, e in [('E1','e1'),('E2','e2')]:
-        rslt['Mean '+name] = df_2_pref.groupby('period')[e].mean().round(3)
-        rslt['SD '+name] = df_2_pref.groupby('period')[e].std().round(3)
+        rslt['Mean '+name] = df_2_pref.groupby('period')[e].mean()
+        rslt['SD '+name] = df_2_pref.groupby('period')[e].std()
         rslt['Min '+name] = df_2_pref.groupby('period')[e].min().astype(int)
         rslt['Max '+name] = df_2_pref.groupby('period')[e].max().astype(int)
     rslt = rslt[['Mean E1', 'SD E1','Mean E2', 'SD E2','Min E1','Min E2','Max E1','Max E2']]
