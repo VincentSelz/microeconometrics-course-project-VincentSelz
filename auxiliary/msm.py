14e0f71f1268fd465be31a87c8adb7cab9b9c649
Vincent Selz <s6viselz@uni-bonn.de>
2020-07-14 00:19:32 +0200

    Outsource empirical moments.

--- auxiliary/msm.py
+++ auxiliary/msm.py
@@ -17,7 +17,7 @@ def params_description():
     'comment':['strength of disappointment aversion on average', 'standard deviation of strength of disappointment aversion',
     'unobserved differences in cost of effort functions that vary over rounds and individuals', 'unobserved differences in cost of effort functions that vary over rounds and individuals',
     'unobserved differences in cost of effort functions between individuals, constant over rounds', 'unobserved differences in cost of effort functions between individuals, constant over rounds',
-    'common cost parameter','common cost parameter','round effects']
+    'common cost parameter','common cost parameter','round effects for round 2,...N']
     }
     df = pd.DataFrame(data, columns=['category', 'name','values', 'comment'])
     df.set_index(['category','name'],inplace=True)
@@ -27,13 +27,13 @@ def trial_values():
     data={
     'name':['lambda','std_lambda', 'phi_pi', 'varphi_pi', 'phi_mu', 'varphi_mu',
     'kappa', 'b','delta2','delta3','delta4','delta5','delta6','delta7','delta8','delta9','delta10'],
-    'trial_values':[2, 1.5,0.35,0.85,0.35,0.85,1.75,np.nan,0, 0, 0, 0, 0, 0, 0, 0,0]
+    'values':[2, 1.5,0.35,0.85,0.35,0.85,1.75,np.nan,0, 0, 0, 0, 0, 0, 0, 0,0]
     }
-    df = pd.DataFrame(data, columns=['name','trial_values'])
+    df = pd.DataFrame(data, columns=['name','values'])
     return df
 
 
-def old_description():
+def moments_description():
     data={'category':['lambda','lambda','lambda','lambda','lambda', 'lambda', 'lambda','std_lambda',
     'std_lambda', 'std_lambda', 'std_lambda', 'std_lambda', 'std_lambda', 'std_lambda',
     'phi_pi', 'phi_pi', 'phi_pi', 'phi_pi', 'phi_pi',
@@ -85,8 +85,9 @@ def old_description():
         'average effort in period i']
         }
 
-    df = pd.DataFrame(data, columns=['category', 'name','values', 'comment'])
-    df.set_index(['category','name'],inplace=True)
+    df = pd.DataFrame(data, columns=['name', 'comment'])
+    df.set_index(['name'],inplace=True)
+    df.drop_duplicates(inplace=True)
     return df
 
 def replace_nans(df):
@@ -214,7 +215,8 @@ def perc17_cond_corr_e2_prize(df):
         p_resid = get_resid(df, i, 'prize')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(p_resid))
-    return np.percentile(corrs, 17)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.17)[0]
 
 def perc33_cond_corr_e2_prize(df):
     """J percentile of the correlation of e2 and the prize after partialing out other effects."""
@@ -223,7 +225,8 @@ def perc33_cond_corr_e2_prize(df):
         p_resid = get_resid(df, i, 'prize')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(p_resid))
-    return np.percentile(corrs, 33)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.33)[0]
 
 def perc50_cond_corr_e2_prize(df):
     """J percentile of the correlation of e2 and the prize after partialing out other effects."""
@@ -232,7 +235,8 @@ def perc50_cond_corr_e2_prize(df):
         p_resid = get_resid(df, i, 'prize')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(p_resid))
-    return np.percentile(corrs, 50)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.5)[0]
 
 def perc66_cond_corr_e2_prize(df):
     """J percentile of the correlation of e2 and the prize after partialing out other effects."""
@@ -241,7 +245,8 @@ def perc66_cond_corr_e2_prize(df):
         p_resid = get_resid(df, i, 'prize')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(p_resid))
-    return np.percentile(corrs, 66)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.66)[0]
 
 def perc83_cond_corr_e2_prize(df):
     """J percentile of the correlation of e2 and the prize after partialing out other effects."""
@@ -250,7 +255,8 @@ def perc83_cond_corr_e2_prize(df):
         p_resid = get_resid(df, i, 'prize')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(p_resid))
-    return np.percentile(corrs, 83)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.83)[0]
 
 def perc17_cond_corr_e2_e1(df):
     """J percentile of the correlation of e2 and e1 after partialing out other effects."""
@@ -259,7 +265,8 @@ def perc17_cond_corr_e2_e1(df):
         e1_resid = get_resid(df, i, 'e1')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(e1_resid))
-    return np.percentile(corrs, 17)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.17)[0]
 
 def perc33_cond_corr_e2_e1(df):
     """J percentile of the correlation of e2 and e1 after partialing out other effects."""
@@ -268,7 +275,8 @@ def perc33_cond_corr_e2_e1(df):
         e1_resid = get_resid(df, i, 'e1')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(e1_resid))
-    return np.percentile(corrs, 33)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.33)[0]
 
 def perc50_cond_corr_e2_e1(df):
     """J percentile of the correlation of e2 and e1 after partialing out other effects."""
@@ -277,7 +285,8 @@ def perc50_cond_corr_e2_e1(df):
         e1_resid = get_resid(df, i, 'e1')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(e1_resid))
-    return np.percentile(corrs, 50)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.5)[0]
 
 def perc66_cond_corr_e2_e1(df):
     """J percentile of the correlation of e2 and e1 after partialing out other effects."""
@@ -286,7 +295,8 @@ def perc66_cond_corr_e2_e1(df):
         e1_resid = get_resid(df, i, 'e1')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(e1_resid))
-    return np.percentile(corrs, 66)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.66)[0]
 
 def perc83_cond_corr_e2_e1(df):
     """J percentile of the correlation of e2 and e1 after partialing out other effects."""
@@ -295,7 +305,8 @@ def perc83_cond_corr_e2_e1(df):
         e1_resid = get_resid(df, i, 'e1')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(e1_resid))
-    return np.percentile(corrs, 83)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.83)[0]
 
 def perc17_cond_corr_e2_e1timesprize(df):
     """J percentile of the correlation of e2 and the e1 * prize after partialing out other effects."""
@@ -304,7 +315,8 @@ def perc17_cond_corr_e2_e1timesprize(df):
         e1timesprize_resid = get_resid(df, i, 'e1timesprize')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(e1timesprize_resid))
-    return np.percentile(corrs, 17)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.17)[0]
 
 def perc33_cond_corr_e2_e1timesprize(df):
     """J percentile of the correlation of e2 and the e1 * prize after partialing out other effects."""
@@ -313,7 +325,8 @@ def perc33_cond_corr_e2_e1timesprize(df):
         e1timesprize_resid = get_resid(df, i, 'e1timesprize')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(e1timesprize_resid))
-    return np.percentile(corrs, 33)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.33)[0]
 
 def perc50_cond_corr_e2_e1timesprize(df):
     """J percentile of the correlation of e2 and the e1 * prize after partialing out other effects."""
@@ -322,7 +335,8 @@ def perc50_cond_corr_e2_e1timesprize(df):
         e1timesprize_resid = get_resid(df, i, 'e1timesprize')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(e1timesprize_resid))
-    return np.percentile(corrs, 50)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.5)[0]
 
 def perc66_cond_corr_e2_e1timesprize(df):
     """J percentile of the correlation of e2 and the e1 * prize after partialing out other effects."""
@@ -331,7 +345,8 @@ def perc66_cond_corr_e2_e1timesprize(df):
         e1timesprize_resid = get_resid(df, i, 'e1timesprize')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(e1timesprize_resid))
-    return np.percentile(corrs, 66)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.66)[0]
 
 def perc83_cond_corr_e2_e1timesprize(df):
     """J percentile of the correlation of e2 and the e1 * prize after partialing out other effects."""
@@ -340,19 +355,20 @@ def perc83_cond_corr_e2_e1timesprize(df):
         e1timesprize_resid = get_resid(df, i, 'e1timesprize')
         e2_resid = get_resid(df, i, 'e2')
         corrs.append(e2_resid.corr(e1timesprize_resid))
-    return np.percentile(corrs, 83)
+        ds = pd.DataFrame(corrs)
+    return ds.quantile(0.83)[0]
 
 def low_effort_low_prize(df):
-    return df.query('e1 < 23 & prize < 1.33')['e2'].mean()
+    return df.query('e1 <= 23 & prize <= 1.33')['e2'].mean()
 
 def low_effort_high_prize(df):
-    return df.query('e1 < 23 & prize > 2.55')['e2'].mean()
+    return df.query('e1 <= 23 & prize >= 2.55')['e2'].mean()
 
 def high_effort_low_prize(df):
-    return df.query('e1 > 28 & prize < 1.33')['e2'].mean()
+    return df.query('e1 >= 28 & prize <= 1.33')['e2'].mean()
 
 def high_effort_high_prize(df):
-    return df.query('e1 > 28 & prize > 2.55')['e2'].mean()
+    return df.query('e1 >= 28 & prize >= 2.55')['e2'].mean()
 
 def low_effort_prop(df):
     return df.query('e2 < 15')['e2'].count()/df['e2'].count()
@@ -374,3 +390,93 @@ def old_percentile_correlation(df):
     for key in dfs:
         cond_corr.append(dfs[key]['e2_resid'].corr(dfs[key]['e1_resid']))
     return np.percentile(cond_corr, 66)
+
+def observed_moments(df):
+    """Compute the observed moments."""
+    observed_moments = {
+        'effort_std': effort_std(df),
+        'corr_round_lag1': pooled_autocorr_lag1(df),
+        'corr_round_lag2': pooled_autocorr_lag2(df),
+        'round_change_std': round_change_std(df),
+        'mean_period_1': period1_average(df),
+        'mean_period_2': period2_average(df),
+        'mean_period_3': period3_average(df),
+        'mean_period_4': period4_average(df),
+        'mean_period_5': period5_average(df),
+        'mean_period_6': period6_average(df),
+        'mean_period_7': period7_average(df),
+        'mean_period_8': period8_average(df),
+        'mean_period_9': period9_average(df),
+        'mean_period_10': period10_average(df),
+        'cond_corr_e2_prize': cond_corr_e2_prize(df),
+        'cond_corr_e2_e1': cond_corr_e2_e1(df),
+        'cond_corr_e2_e1timesprize': cond_corr_e2_e1timesprize(df),
+        'perc17_cond_corr_e2_prize': perc17_cond_corr_e2_prize(df),
+        'perc33_cond_corr_e2_prize': perc33_cond_corr_e2_prize(df),
+        'perc50_cond_corr_e2_prize': perc50_cond_corr_e2_prize(df),
+        'perc66_cond_corr_e2_prize': perc66_cond_corr_e2_prize(df),
+        'perc83_cond_corr_e2_prize': perc83_cond_corr_e2_prize(df),
+        'perc17_cond_corr_e2_e1': perc17_cond_corr_e2_e1(df),
+        'perc33_cond_corr_e2_e1': perc33_cond_corr_e2_e1(df),
+        'perc50_cond_corr_e2_e1': perc50_cond_corr_e2_e1(df),
+        'perc66_cond_corr_e2_e1': perc66_cond_corr_e2_e1(df),
+        'perc83_cond_corr_e2_e1': perc83_cond_corr_e2_e1(df),
+        'perc17_cond_corr_e2_e1timesprize': perc17_cond_corr_e2_e1timesprize(df),
+        'perc33_cond_corr_e2_e1timesprize': perc33_cond_corr_e2_e1timesprize(df),
+        'perc50_cond_corr_e2_e1timesprize': perc50_cond_corr_e2_e1timesprize(df),
+        'perc66_cond_corr_e2_e1timesprize': perc66_cond_corr_e2_e1timesprize(df),
+        'perc83_cond_corr_e2_e1timesprize': perc83_cond_corr_e2_e1timesprize(df),
+        'low_effort_low_prize': low_effort_low_prize(df),
+        'low_effort_high_prize': low_effort_high_prize(df),
+        'high_effort_low_prize': high_effort_low_prize(df),
+        'high_effort_high_prize': high_effort_high_prize(df),
+        'low_effort_prop': low_effort_prop(df),
+        'high_effort_prop': high_effort_prop(df)
+        }
+    return observed_moments
+
+def get_weighting_matrix(
+    data,
+    empirical_moments,
+    calc_moments,
+    n_bootstrap_samples,
+    n_observations_per_sample,
+    replace_missing_variances=None,
+):
+    """ Computes a diagonal weighting matrix for estimation with msm. Weights are the
+    inverse bootstrap variances of the observed sample moments."""
+    # Seed for reproducibility.
+    np.random.seed(47828324)
+    flat_empirical_moments = rp.get_flat_moments(empirical_moments)
+    index_base = data.index.get_level_values("Identifier").unique()
+    calc_moments = _harmonize_input(calc_moments)
+    # Create bootstrapped moments.
+    moments_sample = []
+    for _ in range(n_bootstrap_samples):
+        ids_boot = np.random.choice(
+            index_base, n_observations_per_sample, replace=False
+        )
+        moments_boot = [func(data.loc[ids_boot]) for func in calc_moments]
+        flat_moments_boot = rp.get_flat_moments(moments_boot)
+        flat_moments_boot = flat_moments_boot.reindex_like(flat_empirical_moments)
+        moments_sample.append(flat_moments_boot)
+
+    # Compute variance for each moment and construct diagonal weighting matrix.
+    moments_var = np.array(moments_sample).var(axis=0)
+
+    # The variance of missing moments is nan. Unless a repalcement variance is
+    # specified, their inverse variance will be set to 0.
+    if replace_missing_variances is None:
+        diagonal = moments_var ** (-1)
+        diagonal = np.nan_to_num(diagonal, nan=0)
+        weighting_matrix = np.diag(diagonal)
+    else:
+        moments_var = np.nan_to_num(moments_var, nan=replace_missing_variances)
+        diagonal = moments_var ** (-1)
+        weighting_matrix = np.diag(diagonal)
+
+    # Checks weighting matrix.
+    if np.isnan(weighting_matrix).any() or np.isinf(weighting_matrix).any():
+        raise ValueError("Weighting matrix contains NaNs or infinite values.")
+
+    return weighting_matrix
