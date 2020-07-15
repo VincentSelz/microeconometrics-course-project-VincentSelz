231aa66b29c5f4f7e176da6b560638c62730afd2
Vincent Selz <s6viselz@uni-bonn.de>
2020-07-15 19:37:21 +0200

    Revise params.

--- auxiliary/msm.py
+++ auxiliary/msm.py
@@ -9,29 +9,28 @@ from auxiliary.helper import *
 from linearmodels.panel import PanelOLS
 
 def params_description():
+    """Provides the relevant unknown parameters and trial values.
+
+    The trial values are computed as the mean of the upper and the lower bound which are also reported.
+    """
     data={'category': ['lambda','lambda', 'cost_of_effort', 'cost_of_effort', 'cost_of_effort',
-    'cost_of_effort','cost_of_effort','cost_of_effort','cost_of_effort'],
+    'cost_of_effort','cost_of_effort','cost_of_effort','cost_of_effort','cost_of_effort','cost_of_effort','cost_of_effort','cost_of_effort','cost_of_effort','cost_of_effort','cost_of_effort','cost_of_effort'],
     'name':['lambda','std_lambda', 'phi_pi', 'varphi_pi', 'phi_mu', 'varphi_mu',
-    'kappa', 'b','delta'],
-    'values':[np.nan]*9,
+    'kappa', 'b','delta_2','delta_3','delta_4','delta_5','delta_6','delta_7','delta_8','delta_9','delta_10'],
     'comment':['strength of disappointment aversion on average', 'standard deviation of strength of disappointment aversion',
     'unobserved differences in cost of effort functions that vary over rounds and individuals', 'unobserved differences in cost of effort functions that vary over rounds and individuals',
     'unobserved differences in cost of effort functions between individuals, constant over rounds', 'unobserved differences in cost of effort functions between individuals, constant over rounds',
-    'common cost parameter','common cost parameter','round effects for round 2,...N']
+    'common cost parameter','common cost parameter','round effects for round 2','round effects for round 3','round effects for round 4','round effects for round 5','round effects for round 6','round effects for round 7','round effects for round 8','round effects for round 9','round effects for round 10'],
+    'lower': [1,0.5,0,0.2,0,0.2,1,-35,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    'upper': [3,2.5,0.7,1.5,0.7,1.5,2.5,-20,0,0,0,0,0,0,0,0,0]
     }
-    df = pd.DataFrame(data, columns=['category', 'name','values', 'comment'])
+    df = pd.DataFrame(data, columns=['category', 'name', 'comment','lower','upper'])
     df.set_index(['category','name'],inplace=True)
+    df['value'] = (df['upper'] + df['lower'])/2
+    for i in range(8,17):
+        df.iloc[i,3] = 0
+    df = df[['value','comment','lower','upper']]
     return df
-
-def trial_values():
-    data={
-    'name':['lambda','std_lambda', 'phi_pi', 'varphi_pi', 'phi_mu', 'varphi_mu',
-    'kappa', 'b','delta2','delta3','delta4','delta5','delta6','delta7','delta8','delta9','delta10'],
-    'values':[2, 1.5,0.35,0.85,0.35,0.85,1.75,np.nan,0, 0, 0, 0, 0, 0, 0, 0,0]
-    }
-    df = pd.DataFrame(data, columns=['name','values'])
-    return df
-
 
 def moments_description():
     data={'category':['lambda','lambda','lambda','lambda','lambda', 'lambda', 'lambda','std_lambda',
@@ -50,16 +49,15 @@ def moments_description():
     'effort_std', 'low_effort_prop','high_effort_prop', 'round_change_std', 'j_perc_cond_corr_e2_prize',
     'effort_std', 'low_effort_prop','high_effort_prop', 'autocorrelation_l1', 'autocorrelation_l2', 'j_perc_cond_corr_e2_prize',
     'low_effort_low_prize', 'low_effort_high_prize','high_effort_low_prize','high_effort_high_prize',
-    'period average','period average'
+    'period_average','period_average'
     ],
-    'values': [np.nan]*42,
     'comment':['correlation of e2 and prize, conditional on control effects',
         'correlation of e2 and e1, conditional on control effects',
         'correlation of e2 and e1 * prize, conditional on control effects',
-        'effort conditional on low e2 and low prizes',
-        'effort conditional on low e2 and high prizes',
-        'effort conditional on low e2 and low prizes',
-        'effort conditional on high e2 and high prizes',
+        'effort conditional on low e1 and low prizes',
+        'effort conditional on low e1 and high prizes',
+        'effort conditional on low e1 and low prizes',
+        'effort conditional on high e1 and high prizes',
         'standard deviation of effort', 'probability of low effort',
         'probability of high effort', 'autocorrelation with lag 1',
         'autocorrelation with lag 2',
@@ -406,47 +404,88 @@ def old_percentile_correlation(df):
         cond_corr.append(dfs[key]['e2_resid'].corr(dfs[key]['e1_resid']))
     return np.percentile(cond_corr, 66)
 
+calc_moments = {
+    'effort_std': effort_std,
+    'corr_round_lag1': pooled_autocorr_lag1,
+    'corr_round_lag2': pooled_autocorr_lag2,
+    'round_change_std': round_change_std,
+    'mean_period_1': period1_average,
+    'mean_period_2': period2_average,
+    'mean_period_3': period3_average,
+    'mean_period_4': period4_average,
+    'mean_period_5': period5_average,
+    'mean_period_6': period6_average,
+    'mean_period_7': period7_average,
+    'mean_period_8': period8_average,
+    'mean_period_9': period9_average,
+    'mean_period_10': period10_average,
+    'cond_corr_e2_prize': cond_corr_e2_prize,
+    'cond_corr_e2_e1': cond_corr_e2_e1,
+    'cond_corr_e2_e1timesprize': cond_corr_e2_e1timesprize,
+    'perc17_cond_corr_e2_prize': perc17_cond_corr_e2_prize,
+    'perc33_cond_corr_e2_prize': perc33_cond_corr_e2_prize,
+    'perc50_cond_corr_e2_prize': perc50_cond_corr_e2_prize,
+    'perc66_cond_corr_e2_prize': perc66_cond_corr_e2_prize,
+    'perc83_cond_corr_e2_prize': perc83_cond_corr_e2_prize,
+    'perc17_cond_corr_e2_e1': perc17_cond_corr_e2_e1,
+    'perc33_cond_corr_e2_e1': perc33_cond_corr_e2_e1,
+    'perc50_cond_corr_e2_e1': perc50_cond_corr_e2_e1,
+    'perc66_cond_corr_e2_e1': perc66_cond_corr_e2_e1,
+    'perc83_cond_corr_e2_e1': perc83_cond_corr_e2_e1,
+    'perc17_cond_corr_e2_e1timesprize': perc17_cond_corr_e2_e1timesprize,
+    'perc33_cond_corr_e2_e1timesprize': perc33_cond_corr_e2_e1timesprize,
+    'perc50_cond_corr_e2_e1timesprize': perc50_cond_corr_e2_e1timesprize,
+    'perc66_cond_corr_e2_e1timesprize': perc66_cond_corr_e2_e1timesprize,
+    'perc83_cond_corr_e2_e1timesprize': perc83_cond_corr_e2_e1timesprize,
+    'low_effort_low_prize': low_effort_low_prize,
+    'low_effort_high_prize': low_effort_high_prize,
+    'high_effort_low_prize': high_effort_low_prize,
+    'high_effort_high_prize': high_effort_high_prize,
+    'low_effort_prop': low_effort_prop,
+    'high_effort_prop': high_effort_prop
+}
+
 def observed_moments(df):
     """Compute the observed moments."""
     observed_moments = {
-        'effort_std': effort_std(df),
-        'corr_round_lag1': pooled_autocorr_lag1(df),
-        'corr_round_lag2': pooled_autocorr_lag2(df),
-        'round_change_std': round_change_std(df),
-        'mean_period_1': period1_average(df),
-        'mean_period_2': period2_average(df),
-        'mean_period_3': period3_average(df),
-        'mean_period_4': period4_average(df),
-        'mean_period_5': period5_average(df),
-        'mean_period_6': period6_average(df),
-        'mean_period_7': period7_average(df),
-        'mean_period_8': period8_average(df),
-        'mean_period_9': period9_average(df),
-        'mean_period_10': period10_average(df),
-        'cond_corr_e2_prize': cond_corr_e2_prize(df),
-        'cond_corr_e2_e1': cond_corr_e2_e1(df),
-        'cond_corr_e2_e1timesprize': cond_corr_e2_e1timesprize(df),
-        'perc17_cond_corr_e2_prize': perc17_cond_corr_e2_prize(df),
-        'perc33_cond_corr_e2_prize': perc33_cond_corr_e2_prize(df),
-        'perc50_cond_corr_e2_prize': perc50_cond_corr_e2_prize(df),
-        'perc66_cond_corr_e2_prize': perc66_cond_corr_e2_prize(df),
-        'perc83_cond_corr_e2_prize': perc83_cond_corr_e2_prize(df),
-        'perc17_cond_corr_e2_e1': perc17_cond_corr_e2_e1(df),
-        'perc33_cond_corr_e2_e1': perc33_cond_corr_e2_e1(df),
-        'perc50_cond_corr_e2_e1': perc50_cond_corr_e2_e1(df),
-        'perc66_cond_corr_e2_e1': perc66_cond_corr_e2_e1(df),
-        'perc83_cond_corr_e2_e1': perc83_cond_corr_e2_e1(df),
-        'perc17_cond_corr_e2_e1timesprize': perc17_cond_corr_e2_e1timesprize(df),
-        'perc33_cond_corr_e2_e1timesprize': perc33_cond_corr_e2_e1timesprize(df),
-        'perc50_cond_corr_e2_e1timesprize': perc50_cond_corr_e2_e1timesprize(df),
-        'perc66_cond_corr_e2_e1timesprize': perc66_cond_corr_e2_e1timesprize(df),
-        'perc83_cond_corr_e2_e1timesprize': perc83_cond_corr_e2_e1timesprize(df),
-        'low_effort_low_prize': low_effort_low_prize(df),
-        'low_effort_high_prize': low_effort_high_prize(df),
-        'high_effort_low_prize': high_effort_low_prize(df),
-        'high_effort_high_prize': high_effort_high_prize(df),
-        'low_effort_prop': low_effort_prop(df),
-        'high_effort_prop': high_effort_prop(df)
+        'effort_std': calc_moments['effort_std'](df),
+        'corr_round_lag1': calc_moments['corr_round_lag1'](df),
+        'corr_round_lag2': calc_moments['corr_round_lag2'](df),
+        'round_change_std': calc_moments['round_change_std'](df),
+        'mean_period_1': calc_moments['mean_period_1'](df),
+        'mean_period_2': calc_moments['mean_period_2'](df),
+        'mean_period_3': calc_moments['mean_period_3'](df),
+        'mean_period_4': calc_moments['mean_period_4'](df),
+        'mean_period_5': calc_moments['mean_period_5'](df),
+        'mean_period_6': calc_moments['mean_period_6'](df),
+        'mean_period_7': calc_moments['mean_period_7'](df),
+        'mean_period_8': calc_moments['mean_period_8'](df),
+        'mean_period_9': calc_moments['mean_period_9'](df),
+        'mean_period_10': calc_moments['mean_period_10'](df),
+        'cond_corr_e2_prize': calc_moments['cond_corr_e2_prize'](df),
+        'cond_corr_e2_e1': calc_moments['cond_corr_e2_e1'](df),
+        'cond_corr_e2_e1timesprize': calc_moments['cond_corr_e2_e1timesprize'](df),
+        'perc17_cond_corr_e2_prize': calc_moments['perc17_cond_corr_e2_prize'](df),
+        'perc33_cond_corr_e2_prize': calc_moments['perc33_cond_corr_e2_prize'](df),
+        'perc50_cond_corr_e2_prize': calc_moments['perc50_cond_corr_e2_prize'](df),
+        'perc66_cond_corr_e2_prize': calc_moments['perc66_cond_corr_e2_prize'](df),
+        'perc83_cond_corr_e2_prize': calc_moments['perc83_cond_corr_e2_prize'](df),
+        'perc17_cond_corr_e2_e1': calc_moments['perc17_cond_corr_e2_e1'](df),
+        'perc33_cond_corr_e2_e1': calc_moments['perc33_cond_corr_e2_e1'](df),
+        'perc50_cond_corr_e2_e1': calc_moments['perc50_cond_corr_e2_e1'](df),
+        'perc66_cond_corr_e2_e1': calc_moments['perc66_cond_corr_e2_e1'](df),
+        'perc83_cond_corr_e2_e1': calc_moments['perc83_cond_corr_e2_e1'](df),
+        'perc17_cond_corr_e2_e1timesprize': calc_moments['perc17_cond_corr_e2_e1timesprize'](df),
+        'perc33_cond_corr_e2_e1timesprize': calc_moments['perc33_cond_corr_e2_e1timesprize'](df),
+        'perc50_cond_corr_e2_e1timesprize': calc_moments['perc50_cond_corr_e2_e1timesprize'](df),
+        'perc66_cond_corr_e2_e1timesprize': calc_moments['perc66_cond_corr_e2_e1timesprize'](df),
+        'perc83_cond_corr_e2_e1timesprize': calc_moments['perc83_cond_corr_e2_e1timesprize'](df),
+        'low_effort_low_prize': calc_moments['low_effort_low_prize'](df),
+        'low_effort_high_prize': calc_moments['low_effort_high_prize'](df),
+        'high_effort_low_prize': calc_moments['high_effort_low_prize'](df),
+        'high_effort_high_prize': calc_moments['high_effort_high_prize'](df),
+        'low_effort_prop': calc_moments['low_effort_prop'](df),
+        'high_effort_prop': calc_moments['high_effort_prop'](df)
         }
     return observed_moments
 
