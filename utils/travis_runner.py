a7fa05a3104238511181531b6b3ac17688277991
Vincent Selz <s6viselz@uni-bonn.de>
2020-07-18 17:43:43 +0200

    Circumvent timeout.

--- utils/travis_runner.py
+++ utils/travis_runner.py
@@ -6,5 +6,5 @@ import glob
 if __name__ == '__main__':
 
     for notebook in glob.glob('*.ipynb'):
-        cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=None'.format(notebook)
+        cmd = ' jupyter nbconvert --execute {} --ExecutePreprocessor.enabled=True  --ExecutePreprocessor.timeout=-1'.format(notebook)
         sp.check_call(cmd, shell=True)
